b90424a0f5336b65887ab7664f02495903fb0379
SahilPandhade <sahilpandhade@rediffmail.com>
2023-09-28 19:18:51 +0530

    Minor fixes

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,18 +1,43 @@
 import React, { ChangeEvent, useState } from 'react'
 
+interface formType {
+    fullName: string,
+    email: string,
+    country?: string,
+    contactNumber?: string,
+    requirement: string
+}
 const ContactForm = () => {
-    const [formState, setFormState] = useState({
+    const [formState, setFormState] = useState<formType>({
         fullName: '',
         email: '',
         country: '',
         contactNumber: '',
         requirement: '',
     });
+    const [validationErrors, setValidationErrors] = useState({
+        fullName: false,
+        email: false,
+        requirement: false,
+    });
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        const errors = {
+            fullName: formState.fullName === '',
+            email: formState.email === '',
+            requirement: formState.requirement === '',
+        };
+
+        setValidationErrors(errors);
+
+        if (Object.values(errors).some((error) => error)) {
+            console.log('Please fill out the mandatory fields.');
+            return;
+        }
         console.log('Form values:', formState);
     };
-    const handleInputChange = (e:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setFormState({
             ...formState,
@@ -22,7 +47,7 @@ const ContactForm = () => {
     return (
         <div className="text-black container mx-auto p-10 bg-gradient-to-b from-white to-gray-300 w-100 rounded-2xl flex flex-col gap-5">
             <div className='flex flex-col gap-2'>
-                <h2 className='font-bold'>Fill out the form</h2>
+                <h2 className='font-bold text-[18px]'>Fill out the form</h2>
                 <h3 className='text-[12px]'>Our team will touch base with you within 24 hours.</h3>
             </div>
             <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4">
@@ -30,7 +55,8 @@ const ContactForm = () => {
                     <input
                         type="text"
                         name="fullName"
-                        className="bg-transparent w-full p-2 border-b border-gray-500 placeholder-gray-400 focus:outline-none"
+                        className={`bg-transparent w-full p-2 border-b border-gray-500 placeholder-gray-400 focus:outline-none
+                         ${validationErrors.fullName ? 'border-red-500' : ''}`}
                         onChange={handleInputChange}
                         placeholder="Full Name*"
                     />
@@ -39,7 +65,8 @@ const ContactForm = () => {
                     <input
                         type="email"
                         name="email"
-                        className="bg-transparent w-full p-2 border-b border-gray-500 placeholder-gray-400 focus:outline-none"
+                        className={`bg-transparent w-full p-2 border-b border-gray-500 placeholder-gray-400 focus:outline-none
+                         ${validationErrors.email ? 'border-red-500' : ''}`}
                         onChange={handleInputChange}
                         placeholder="Email ID*"
                     />
@@ -65,13 +92,14 @@ const ContactForm = () => {
                 <div className="col-span-2">
                     <textarea
                         name='requirement'
-                        className="bg-transparent w-full p-2 border-b border-gray-500 placeholder-gray-400 focus:outline-none"
+                        className={`bg-transparent w-full p-2 border-b border-gray-500 placeholder-gray-400 focus:outline-none
+                        ${validationErrors.email ? 'border-red-500' : ''}`}
                         rows={4}
                         onChange={handleInputChange}
                         placeholder="Tell us your requirement*"
                     ></textarea>
                 </div>
-            </form>
+            
             <div className="flex gap-2">
                 <button type="submit" className="bg-gray-900 text-white py-2 px-4 rounded-3xl text-[12px] p-4 hover:bg-gray-500">
                     Submit
@@ -80,6 +108,7 @@ const ContactForm = () => {
                     <span>&rarr;</span>
                 </button>
             </div>
+            </form>
         </div>
     )
 }
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -9,7 +9,7 @@ const MainPage = () => {
             <div className={`${st.paddingX} main mt-12 inset-0 top-[150px]  mx-auto flex flex-row items-start gap-10 xs:flex-col`}>
                 <div className='p-5 flex flex-col gap-5'>
                     <h1 className='text-[42px]'>Searching for <b>Augmented Development</b> Teams to steer your product towards triumph?</h1>
-                    <ul className='flex flex-row gap-10'>
+                    <ul className='flex gap-12 flex-row'>
                         <li><label><p className='text-[32px] font-bold'>50+</p> Clients</label></li>
                         <li><label><p className='text-[32px] font-bold'>80+</p> Projects successfully completed</label></li>
                         <li><label><p className='text-[32px] font-bold'>60%</p> of the clients converted into longterm engagement partners</label></li>
